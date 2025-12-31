a08b7c083ff547960929d2f00fccbb8df7798dad
lidor4x4 <lidor4x4@gmail.com>
2025-12-31 10:48:20 +0200

    Added video with two ppl

--- screens/live_chat_screen.py
+++ screens/live_chat_screen.py
@@ -58,17 +58,3 @@ class LiveChatPanel(wx.Panel):
             h, w = img.shape[:2]
             bmp = wx.Bitmap.FromBuffer(w, h, img)
             wx.CallAfter(lambda: [self.remote_video.SetBitmap(bmp), self.Layout()])
-
-    def send_audio(self):
-        def callback(indata, frames, time, status):
-            self.audio_udp.sendto(indata.tobytes(), (self.server_ip, AUDIO_PORT))
-        with sd.InputStream(channels=1, samplerate=44100, callback=callback):
-            while True:
-                sd.sleep(1000)
-
-    def receive_audio(self):
-        stream = sd.OutputStream(channels=1, samplerate=44100)
-        stream.start()
-        while True:
-            data, _ = self.audio_udp.recvfrom(4096)
-            stream.write(np.frombuffer(data, dtype=np.float32))
--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-import wx
-from screen_manager import MainFrame
 
+"""
+    def send_audio(self):
+        def callback(indata, frames, time, status):
+            self.audio_udp.sendto(indata.tobytes(), (self.server_ip, AUDIO_PORT))
+        with sd.InputStream(channels=1, samplerate=44100, callback=callback):
+            while True:
+                sd.sleep(1000)
 
-class MyApp(wx.App):
-    
-    def OnInit(self):
-        frame = MainFrame()
-        frame.Center()
-        frame.Show()
-        return True
-
-
-if __name__ == "__main__":
-    app = MyApp()
-    app.MainLoop()
+    def receive_audio(self):
+        stream = sd.OutputStream(channels=1, samplerate=44100)
+        stream.start()
+        while True:
+            data, _ = self.audio_udp.recvfrom(4096)
+            stream.write(np.frombuffer(data, dtype=np.float32))
+"""
